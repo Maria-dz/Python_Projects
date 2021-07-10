c48757c015966e439f253c37412c896ce1cc7436
Maria-dz <67874681+Maria-dz@users.noreply.github.com>
2021-07-10 18:41:37 +0300

    Delete decorator_to_json.py

--- decorator_to_json.py
+++
@@ -1,9 +0,0 @@
-import json
-from functools import wraps
-
-def to_json(func):      #декоратор, превращающий результат в формат JSON
-    @wraps(func)
-    def wrapped(*args, **kwargs):
-        result = func(*args,**kwargs)
-        return json.dumps(result)
-    return wrapped
